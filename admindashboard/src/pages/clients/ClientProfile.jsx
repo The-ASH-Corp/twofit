d6dacc76b15a036f051aebb0e6896dda860d2900
shebin-tvk <shebintvk@gmail.com>
2025-12-22 17:12:40 +0530

    client profile backend integrated

--- admindashboard/src/pages/clients/ClientProfile.jsx
+++ admindashboard/src/pages/clients/ClientProfile.jsx
@@ -2,36 +2,38 @@ import React, { useEffect } from "react";
 import ProfileLeftSide from "@/components/clients/ProfileLeftSide";
 import ProfileCenterSide from "@/components/clients/ProfileCenterSide";
 import ProfileRightSide from "@/components/clients/ProfileRightSide";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getClient } from "@/redux/features/client/client.thunk";
 import { useParams } from "react-router-dom";
-
+import {
+  selectSelectedClient,
+  selectClientStatus,
+  selectClientError,
+} from "@/redux/features/client/client.selectors";
 
 const ClientProfile = () => {
-  let data = null
-
   const dispatch = useDispatch();
-  const { clientId } = useParams()
+  const { clientId } = useParams();
 
-  const handleLogin =async () => {    
-      await  dispatch(getClient(clientId));
-  
-    };
+  const client = useSelector(selectSelectedClient);
+  const status = useSelector(selectClientStatus);
+  const error = useSelector(selectClientError);
 
- 
-  
+  useEffect(() => {
+    if (clientId) {
+      dispatch(getClient({ id: clientId }));
+      // console.log(client)
+    }
+  }, [clientId, dispatch]);
 
- 
+  if (status === "loading") return <p>Loading...</p>;
+  if (error) return <p className="text-red-500">{error}</p>;
 
-  
   return (
     <div className="flex justify-between w-full gap-4 h-[calc(100vh-120px)]">
-      {/* left */}
-      <ProfileLeftSide />
-      {/* center */}
-      <ProfileCenterSide />
-      {/* right */}
-      <ProfileRightSide />
+      <ProfileLeftSide client={client} />
+      <ProfileCenterSide client={client} />
+      <ProfileRightSide client={client} />
     </div>
   );
 };
