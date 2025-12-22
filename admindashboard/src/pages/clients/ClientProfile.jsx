76059c4fc67f64468689b74fc88460fbcc4cbd89
shebin-tvk <shebintvk@gmail.com>
2025-12-22 10:01:42 +0530

    first push

--- admindashboard/src/pages/clients/ClientProfile.jsx
+++ admindashboard/src/pages/clients/ClientProfile.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ProfileLeftSide from "@/components/clients/ProfileLeftSide";
 import ProfileCenterSide from "@/components/clients/ProfileCenterSide";
 import ProfileRightSide from "@/components/clients/ProfileRightSide";
+import { useDispatch } from "react-redux";
+import { getClient } from "@/redux/features/client/client.thunk";
+import { useParams } from "react-router-dom";
 
 
 const ClientProfile = () => {
+  let data = null
+
+  const dispatch = useDispatch();
+  const { clientId } = useParams()
+
+  const handleLogin =async () => {    
+      await  dispatch(getClient(clientId));
+  
+    };
+
+ 
+  
+
+ 
 
   
   return (
