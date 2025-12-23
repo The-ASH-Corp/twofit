c9d7006728170e264231e0eeb65c1cc27ef219f5
Arjun <arjunvengassery123@gmail.com>
2025-12-23 16:51:15 +0530

    feat: Add ExpertTable component to admindashboard and relocate package-lock.json.

--- admindashboard/src/pages/experts/ExpertTable.jsx
+++ admindashboard/src/pages/experts/ExpertTable.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import BaseTable from '../../components/table/BaseTable'
 import { ExpertColumns } from './ExpertColumns'
-import { expertsData } from './expertData'
+import { useDispatch } from 'react-redux';
+import { getAllCoaches } from '@/redux/features/coach/coach.thunk';
 
 export default function ExpertTable() {
+
+  const [coaches,setCoaches]=useState([])
+  const page =1
+  const limit =10
+  const dispatch = useDispatch();
+  const fetchCoachData=async()=>{
+    const coache =await dispatch(getAllCoaches({page,limit})).unwrap()
+    setCoaches(coache)
+  }
+
+  useEffect(() => {
+    fetchCoachData();
+  }, []);
+  
   return (
     <div>
       <BaseTable
         columns={ExpertColumns}
-        data={expertsData}
+        data={coaches}
         actionLabel="Add Expert"
         actionPath="/addexpert"
         profilePath="/experts/profile/:expertId"
