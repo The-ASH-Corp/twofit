5897d510bf876d2c30aa1849654114796bb5d26c
shebin-tvk <shebintvk@gmail.com>
2025-12-26 15:50:42 +0530

    expert profile backend added

--- admindashboard/src/redux/features/coach/coach.selector.js
+++ admindashboard/src/redux/features/coach/coach.selector.js
@@ -1,3 +1,4 @@
 export const selectAllCoaches = (state) => state.coach.allCoaches;
+export const selectCoachById = (state) => state.coach.selectedCoach;
 export const selectCoachError = (state) => state.coach.error;
 export const selectCoachStatus = (state) => state.coach.status;
