4d121506a738a638b1ef273d1030dd779fffa41e
Arjun <arjunvengassery123@gmail.com>
2026-01-02 14:22:44 +0530

    feat: Update AdminProfile to include AdminRightSide component and enhance ClientsTable and ExpertTable with pagination and search functionality

--- admindashboard/src/pages/head/clients/ClientsTable.jsx
+++ admindashboard/src/pages/head/clients/ClientsTable.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import BaseTable from "../../../components/table/BaseTable";
 import { ClientColumns } from "./ClientColumns";
 import { useDispatch } from "react-redux";
@@ -12,20 +12,48 @@ import { getAllClients } from "@/redux/features/client/client.thunk";
 import { useNavigate } from "react-router-dom";
 
 export default function ClientsTable() {
-  const navigate = useNavigate();
-  const profilePath = (id)=> {
-        navigate(`/head/clients/profile/${id}`);
-  }
-  const dispatch = useDispatch();
-
-  const clients = useAppSelector(selectAllClients);
   const status = useAppSelector(selectClientStatus);
   const error = useAppSelector(selectClientError);
+  const [clients, setClients] = useState([]);
+
+  const [page, setPage] = useState(1);
+  const [limit, setLimit] = useState(10);
+
+  const fetchClientData = async () => {
+    const client = await dispatch(getAllClients({ page, limit })).unwrap();
+    setClients(client);
+  };
+
+  const navigate = useNavigate();
+
+  const handlePageChange = (newPage) => {
+    setPage(newPage);
+  };
+
+  const handleLimitChange = (newLimit) => {
+    setLimit(newLimit);
+  };
+
+  const searchInpiutHandler = (e) => {
+    const value = e.target.value.toLowerCase();
+    const filteredAdmins = clients.filter((admin) => {
+      return admin.name.toLowerCase().includes(value);
+    });
+    setClients(filteredAdmins);
+    if (value == "") {
+      fetchClientData();
+    }
+  };
+
+  const profilePath = (id) => {
+    navigate(`/head/clients/profile/${id}`);
+  };
+
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(getAllClients({ page: 1, limit: 10 }));
-
-  }, [dispatch]);
+    fetchClientData();
+  }, [page, limit]);
 
   if (status === "loading") return <p>Loading clients...</p>;
   if (error) return <p>{error}</p>;
@@ -35,8 +63,13 @@ export default function ClientsTable() {
       <BaseTable
         columns={ClientColumns}
         data={clients}
-        profilePath= {profilePath}
+        profilePath={profilePath}
         pageLabel={"Clients"}
+        onSearchInputChange={searchInpiutHandler}
+        handlePageChange={handlePageChange}
+        handleLimitChange={handleLimitChange}
+        page={page}
+        limit={limit}
       />
     </div>
   );
