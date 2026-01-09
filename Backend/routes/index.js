25312bb9ac0557bf5c44dc69bec687f3e58dff8c
arjun7736 <arjunvengassery123@gmail.com>
2026-01-09 16:55:28 +0530

    refactor: Remove workout and workoutTemplate modules; clean up unused code and routes

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -1,6 +1,5 @@
 import express from "express"
 import coachRoutes from "../modules/coach/coach.route.js"
-import workoutRoutes from "../modules/workoutTemplate/workout.routes.js"
 import programRoutes from "../modules/allPrograms/allPrograma.route.js"
 import authRoutes from '../modules/auth/auth.routes.js'
 import clientRoutes from "../modules/clients/client.routes.js"
@@ -16,7 +15,6 @@ const router = express.Router();
 router.use('/',authRoutes)
 router.use("/clients",clientRoutes)
 router.use("/coach", coachRoutes);
-router.use("/workout", workoutRoutes);
 router.use("/programs", programRoutes);
 router.use("/chats",chatRoutes)
 router.use("/therapy", therapyRouts);
