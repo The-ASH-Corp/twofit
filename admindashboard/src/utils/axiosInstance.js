b98c91e6f095bdf81f066ee6cda3264c5c42f0cf
arjun7736 <arjunvengassery123@gmail.com>
2026-01-21 14:06:18 +0530

    refactor: Improve code structure and readability in various files

--- admindashboard/src/utils/axiosInstance.js
+++ admindashboard/src/utils/axiosInstance.js
@@ -15,35 +15,38 @@ export const axiosFile = axios.create({
 
 
 // Request Interceptor
-axiosInstance.interceptors.request.use(
-  (config) => {
-    const token =localStorage.getItem("token");
-    if (token) {
-      config.headers.authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
+const requestInterceptor = (config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.authorization = `Bearer ${token}`;
+  }
+  return config;
+};
+
+const requestErrorInterceptor = (error) => Promise.reject(error);
+
+axiosInstance.interceptors.request.use(requestInterceptor, requestErrorInterceptor);
+axiosFile.interceptors.request.use(requestInterceptor, requestErrorInterceptor);
 
 // Response Interceptor
-axiosInstance.interceptors.response.use(
-  (response) => {
-    const newAccessToken = response.headers["x-access-token"];
-    if (newAccessToken) {
-      localStorage.setItem("token", newAccessToken);
-    }
-    return response.data;
-  },
-  (error) => {
-    if (error.response?.status === 401) {
-      // logout or refresh token logic can go here
-      // For now, if we get 401, it likely means the refresh token also failed or wasn't present
-      // localStorage.removeItem("token");
-      // window.location.href = "/login"; 
-    }
-    return Promise.reject(error);
+const responseInterceptor = (response) => {
+  const newAccessToken = response.headers["x-access-token"];
+  if (newAccessToken) {
+    localStorage.setItem("token", newAccessToken);
   }
-);
+  return response.data;
+};
+
+const responseErrorInterceptor = (error) => {
+  if (error.response?.status === 401) {
+    // Optional: handle global logout or redirect to login
+    // localStorage.removeItem("token");
+    // window.location.href = "/login";
+  }
+  return Promise.reject(error);
+};
+
+axiosInstance.interceptors.response.use(responseInterceptor, responseErrorInterceptor);
+axiosFile.interceptors.response.use(responseInterceptor, responseErrorInterceptor);
 
 export default axiosInstance;
