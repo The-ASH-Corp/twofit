fbb7117ede30002215e692c20d40518275ff5185
arjun7736 <arjunvengassery123@gmail.com>
2026-02-09 15:27:27 +0530

    Fix: update CORS origin to include additional localhost address

--- Backend/index.js
+++ Backend/index.js
@@ -25,7 +25,7 @@ app.use(cookieParser())
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5000"],
+    origin: ["http://localhost:5173", "http://159.89.166.107:5173", "http://localhost:5000"],
     credentials: true,
     exposedHeaders: ["x-access-token"],
   })
