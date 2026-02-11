f5bf21a1bbc060d50971ba47f1e61182c063bea3
bencyfathima <biyyubencyali@gmail.com>
2026-02-11 11:54:35 +0530

    added

--- admindashboard/src/pages/expert/habit/HabitStartButton.jsx
+++ admindashboard/src/pages/expert/habit/HabitStartButton.jsx
@@ -3,10 +3,10 @@ import { useNavigate } from "react-router-dom";
 export default function HabitStartButton({ clientId }) {
   const navigate = useNavigate();
 
-  const handleClick = (e) => {
-    e.stopPropagation();  
-    navigate(`add-habit/${clientId}`);
-  };
+  // const handleClick = (e) => {
+  //   e.stopPropagation();  
+  //   navigate(`add-habit/${clientId}`);
+  // };
 
   return (
     <div>
