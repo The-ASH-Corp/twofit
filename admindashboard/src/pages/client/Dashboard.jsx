ba8e4edd401c7e1543b4b79297a1c76d82efd803
bencyfathima <biyyubencyali@gmail.com>
2026-02-13 15:48:59 +0530

    added

--- admindashboard/src/pages/client/Dashboard.jsx
+++ admindashboard/src/pages/client/Dashboard.jsx
@@ -187,6 +187,7 @@ export default function Dashboard() {
           <ExpertsList expert={coaches} />
           <Measeurement />
           <NotificationsList />
+          
         </div>
       </div>
 
