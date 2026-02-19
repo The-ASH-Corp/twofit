f8bb18fb8c604cd69bae9243f764d13b03ca371d
bencyfathima <biyyubencyali@gmail.com>
2026-02-19 10:09:53 +0530

    added

--- admindashboard/src/App.jsx
+++ admindashboard/src/App.jsx
@@ -253,8 +253,8 @@ function App() {
           <Route path="clients" element={<AdminClientsTable />} />
           <Route path="experts" element={<AdminExpertTable />} />
           <Route path="chats" element={<AdminChats />} />
-          <Route path="experts/edit/:id" element={<AdminExpertEdit />} />
-          <Route path="experts/delete/:id" element={<AdminExpertDelete />} />
+          <Route path="experts/edit/:id" element={<AdminExpertEdit />} /> 
+           <Route path="experts/delete/:id" element={<AdminExpertDelete />} /> 
           <Route
             path="experts/profile/:expertId"
             element={<AdminExpertProfile />}
