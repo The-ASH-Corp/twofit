7bc9be631ded4e7febb220f1606a7a5350c19ba1
shebin-tvk <shebintvk@gmail.com>
2026-03-11 12:29:31 +0530

    first commit

--- admindashboard/src/pages/admin/experts/ExpertColumns.jsx
+++ admindashboard/src/pages/admin/experts/ExpertColumns.jsx
@@ -16,25 +16,7 @@ const statusColors = {
 };
 
 export const ExpertColumns = [
-  {
-    id: "select",
-    header: ({ table }) => (
-      <input
-        type="checkbox"
-        checked={table.getIsAllRowsSelected()}
-        onChange={table.getToggleAllRowsSelectedHandler()}
-        className="w-3 h-3 cursor-pointer"
-      />
-    ),
-    cell: ({ row }) => (
-      <input
-        type="checkbox"
-        checked={row.getIsSelected()}
-        onChange={row.getToggleSelectedHandler()}
-        className="w-3 h-3 cursor-pointer"
-      />
-    ),
-  },
+
   { accessorKey: "name", header: "Expert Name" },
   {
     accessorKey: "role",
