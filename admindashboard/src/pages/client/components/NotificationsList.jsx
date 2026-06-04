ba7c17508d0cf2663d806e9a8ba0e40784738f48
shebin-tvk <shebintvk@gmail.com>
2026-06-04 15:06:54 +0530

    first commit

--- admindashboard/src/pages/client/components/NotificationsList.jsx
+++ admindashboard/src/pages/client/components/NotificationsList.jsx
@@ -58,6 +58,7 @@ export default function NotificationsList() {
                     formatNotificationTime(notification.createdAt)}
                 </p>
               </div>
+              0
             </div>
           ))
         )}
